cfc249abf9d536f275d98b5e2eb91723c079d012
jdc996 <jdcubillos96@gmail.com>
2019-08-30 15:05:06 -0500

    Se agrega animacion al momento de abrir y cerrar el menu, se cambia la direccion del servicio que trae las aplicaciones de acuerdo al rol de la persona, con @fabianleon @FredyRodriguezR

--- menu.js
+++ menu.js
@@ -1,15 +1,16 @@
-
-var x = [];
-var google = {
-    url: "www.google.com",
-    image_url: "https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/la-fundacion-de-google.png",
-    nombre: "google"
-}
 var userAction = function () {
     console.log("calling");
+    var data = [{Nombre: 'CONTRATISTA'},{Nombre:"ESTUDIANTE"},{Nombre:"SUPERVISOR"}];
     var promesa = new Promise(function (resolve, reject) {
         document.getElementById("loading").setAttribute("class", "visible")
-        fetch('http://10.20.0.254/configuracion_api/v1/aplicacion?limit=-1')
+        fetch('http://pruebasapi.intranetoas.udistrital.edu.co:8086/v1/aplicacion_rol/aplicacion_rol',{
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data), // data can be string or {object}!
+            headers:{
+              'Content-Type': 'application/json'
+            }
+          }
+        )
             .then(function (data) {
                 console.log("resolve")
                 document.getElementById("loading").setAttribute("class", "hidden");
@@ -25,11 +26,12 @@ function drawMenuApp() {
     document.getElementById("refresh").setAttribute("class", "hidden")
     userAction().then(function (res) {
         res.forEach(function (element) {
-            console.table(element)
+            //console.table(element)
             var app = document.createElement('div');
             var etiqueta = document.createElement('a');
             var img = document.createElement('img');
             var txt =document.createElement('span');
+            //console.log("txt",txt)
             app.setAttribute("class", "menu_item")
             //lista.setAttribute("class", "list-group item")
             etiqueta.appendChild(img)
@@ -37,7 +39,8 @@ function drawMenuApp() {
             etiqueta.setAttribute("href", "http://" + element.Dominio)
             img.setAttribute("class", "menu-app")
             img.setAttribute("id",element.EstiloIcono)
-            txt.setAttribute("class","menu-txt-app")                            
+            txt.setAttribute("class","menu-txt-app");                
+            
             //En caso de que el elemento tenga el atributo de alias vacio se agreguegara un espacio en blanco en el menu para no descuadrar el flex
             (element.Alias==!""?txt.appendChild(document.createTextNode(element.Alias)):txt.appendChild(document.createTextNode("\u00A0")))
             app.appendChild(txt);
@@ -47,22 +50,20 @@ function drawMenuApp() {
 
     })
         .catch(function (e) {
+            console.log(e);
             document.getElementById("loading").setAttribute("class", "hidden")
             document.getElementById("refresh").setAttribute("class", "visible")
         });
 }
 
-// function createComponents(obj){
-//     obj.forEach(function(element){
-//         document.createElement(element)
-
-//     })
-// }
 drawMenuApp();
 
-// myJson.then((response)=>{
-//   console.log(response);
-// }).catch((error)=>{
-//   console.log(error)
-// });
-
+function abrirMenu(){
+    var menu=document.getElementById('menu')
+    if (!menu.className.includes("menu_is_active")){
+        console.log("entro a funcion")
+        menu.classList.add('menu_is_active')
+    }else{
+        menu.classList.remove('menu_is_active')
+    }
+}
